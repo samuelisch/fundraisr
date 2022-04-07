5209e55053a822b4c2cca68f682b989f2d0bbccd
yeogh <52830435+yeogh@users.noreply.github.com>
2022-04-07 16:35:51 +0800

    tested fetching allUsers route

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,17 +1,23 @@
 import React, {useState} from 'react';
-import callapi from '../callapi'
+import callApi from '../callApi'
 
 const Homepage = () => {
 
     const [usersList, setUsersList] = useState([]);
 
-    
+    const allUsers = async (event) => {
+        event.preventDefault();
+        const allUsersData = await callApi.allUsers();
+        console.log(allUsersData)
+        setUsersList(allUsersData)
+     }
 
+    console.log(usersList)
 
     return (
         <div>
             <h1>Homepage</h1>
-            <form action="/users" method="get">
+            <form onSubmit={allUsers}>
               <button type="submit">All Users</button>
           </form>
 
