977cce5dc968f60dcbb3fead4742ec755ab64c9d
yeogh <52830435+yeogh@users.noreply.github.com>
2022-04-08 17:44:56 +0800

    tested fetching allCampaigns route

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -6,12 +6,19 @@ import Button from "./assets/Button";
 
 const Homepage = () => {
   const [usersList, setUsersList] = useState([]);
+  const [campaignList, setCampaignList] = useState([]);
 
   useEffect(() => {
     if (usersList.length) {
       console.log(usersList);
     }
   }, [usersList]);
+
+  useEffect(() => {
+    if (campaignList.length) {
+      console.log(campaignList);
+    }
+  }, [campaignList]);
 
   const allUsers = async (event) => {
     event.preventDefault();
@@ -23,6 +30,12 @@ const Homepage = () => {
     event.preventDefault();
     const singleUserData = await callApi.singleUser(id);
     console.log(singleUserData);
+  };
+
+  const allCampaigns = async (event) => {
+    event.preventDefault();
+    const allCampaignsData = await callApi.allCampaigns();
+    setCampaignList(allCampaignsData);
   };
 
   return (
@@ -45,6 +58,7 @@ const Homepage = () => {
             type="button"
             text="Donate"
             className="h-10 px-5 bg-white text-blue-700 transition-colors duration-150 rounded-lg focus:shadow-outline hover:bg-blue-500 hover:text-blue-100 absolute font-bold left-10 top-10"
+            clickHandler={allCampaigns}
           />
         </div>
 
