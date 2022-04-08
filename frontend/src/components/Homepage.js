3d7f23747662c84e11bae8402e26c24033a4525e
yeogh <52830435+yeogh@users.noreply.github.com>
2022-04-08 15:14:39 +0800

    Added image folder and amended homepage to display 2 main options

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import callApi from "../callApi";
 import Signup from "./Signup";
 import Login from "./Login";
+import mainleft from "../components/assets/images/mainleft.jpg"
+import mainright from "../components/assets/images/mainright.jpg"
+import Button from "./assets/Button";
 
 const Homepage = () => {
   const [usersList, setUsersList] = useState([]);
@@ -24,7 +27,28 @@ const Homepage = () => {
 
   return (
     <div>
-      <h1 className="font-bold">Homepage</h1>
+      <h1 className="font-bold text-center">Support a cause, show you care.</h1>
+      <h1 className="text-center">I want to...</h1>
+
+      <div className="grid grid-cols-2 gap-4 flex items-center">
+  <div className="relative h-25">
+    <img src={mainleft} className="max-w-full object-contain rounded-lg" alt=""/>
+    <Button 
+    type="button"
+    text="Donate"
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-0"/>
+  </div>
+
+  <div className="relative h-25">
+    <img src={mainright} className=" object-contain rounded-lg" alt=""/>
+    <Button 
+    type="button"
+    text="Start a Campaign"
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-0"/>
+  </div>
+ 
+</div>
+
       <div className="flex flex-col space-y-4">
         <form onSubmit={allUsers}>
           <button
