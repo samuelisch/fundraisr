0274d942668182c581455c8b08244d1aef530ec6
samuelisch <samuelchanwk@hotmail.com>
2022-04-12 09:58:53 +0800

    replaces file name in backend creation from colons to dash

--- frontend/src/components/NewCampaign.js
+++ frontend/src/components/NewCampaign.js
@@ -34,7 +34,6 @@ const NewCampaign = () => {
   let buttonStyle = "inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out";
 
   const handleTagsChange = (event) => {
-    
     if (!tags.includes(event.target.value)) {
       event.target.className = "bg-blue-600 inline-block px-6 py-2 border-2 border-blue-600 text-white font-medium text-xs leading-tight rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out";      
       setTags((prevState) => {
