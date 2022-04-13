d8c9a196b7893b4fcda7f29f66d80a5459ffb7dd
yeogh <52830435+yeogh@users.noreply.github.com>
2022-04-13 17:18:16 +0800

    styling for profile page

--- frontend/src/components/UserEditForm.js
+++ frontend/src/components/UserEditForm.js
@@ -4,31 +4,34 @@ import Input from './assets/Input';
 
 const UserEditForm = ({ name, email, changeNameHandler, changeEmailHandler, handleSubmit }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="nameEdit">Name:</label>
-      <Input
-        className="border-2 border-blue-600 rounded-lg p-1  m-3"
-        type="text"
-        name="nameEdit"
-        value={name}
-        placeholder="Name"
-        changeHandler={(e) => changeNameHandler(e.target.value)}
-        minLength={2}
-      />
-      <label htmlFor="emailEdit">Email:</label>
-      <Input 
-        className="border-2 border-blue-600 rounded-lg p-1  m-3"
-        type="email"
-        name="emailEdit"
-        value={email}
-        placeholder=" New email"
-        changeHandler={(e) => changeEmailHandler(e.target.value)}
-        minLength={3}
-      />
-      <Button 
-        className="bg-blue-500 text-ml hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded"
+  <>
+    <h1 className="text-base font-bold p-2 text-gray-700">Update Profile</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nameEdit" className="text-gray-700 ml-5">Name:</label>
+          <Input
+          className="border-2 border-blue-600 rounded-lg p-1  m-3"
+          type="text"
+          name="nameEdit"
+          value={name}
+          placeholder="Name"
+          changeHandler={(e) => changeNameHandler(e.target.value)}
+          minLength={2}
+        />
+        <label htmlFor="emailEdit" className="text-gray-700 ml-5">Email:</label>
+        <Input 
+          className="border-2 border-blue-600 rounded-lg p-1  m-3"
+          type="email"
+          name="emailEdit"
+          value={email}
+          placeholder=" New email"
+          changeHandler={(e) => changeEmailHandler(e.target.value)}
+          minLength={3}
+        />
+        <Button 
+        className="btn rounded-lg bg-primary hover:bg-primary/70 border-none text-white normal-case ml-5" 
         type="submit" text="Update profile" />
-    </form>
+      </form>
+    </>
   )
 }
 
